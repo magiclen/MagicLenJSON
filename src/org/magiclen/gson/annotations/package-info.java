7fc022964da64a9e641f81df6bf8ef154468df55
Magic Len <len@magiclen.org>
2017-06-12 01:47:58 +0800

    Release 2.3.0

---
+++ src/org/magiclen/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link org.magiclen.gson.Gson}.
+ *
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package org.magiclen.gson.annotations;
---
+++ src/org/magiclen/gson/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This package provides the {@link org.magiclen.gson.Gson} class to convert Json
+ * to Java and vice-versa.
+ *
+ * <p>
+ * The primary class to use is {@link org.magiclen.gson.Gson} which can be
+ * constructed with {@code new Gson()} (using default settings) or by using
+ * {@link org.magiclen.gson.GsonBuilder} (to configure various options such as
+ * using versioning and so on).</p>
+ *
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package org.magiclen.gson;
